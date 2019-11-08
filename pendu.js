511aedead358aa944e2116e5ff50562e3a31f3d7
Jonathan Petroons <petroons.jonathan@gmail.com>
2019-11-08 09:42:10 +0100

    check

--- pendu.js
+++ pendu.js
@@ -1,15 +1,16 @@
 let game = () => {
-    const WORD = ['B', 'O', 'N', 'J', 'O', 'U', 'R'];
-    let guessWord = ['_', '_', '_', '_', '_', '_', '_'];
+    const WORD = ["B","O","N","J","O","U","R"];
+    let guessWord = ["_", "_", "_", "_", "_", "_", "_"];
+    let fail = [];
     let finish = [];
     let test;
     let count = 0;
     let guessLetter = () => {
         test = 0;
-        let enterLetter = prompt('Entrez une lettre afin de deviner le mot.').toUpperCase();
+        let enterLetter = prompt("Veuillez entrer une lettre pour commencer à jouer.").toUpperCase();
         for(let i = 0; i <= finish.length; i++){
             if(finish[i] === enterLetter){
-                alert('deja dit');
+                alert("Lettre déjà utilisée, veuillez entrer une autre lettre.");
                 guessLetter();
                 count ++;
             }
@@ -23,14 +24,23 @@ let game = () => {
             }
         }
         if(test == 1){
-            alert(`bonne réponse: ${guessWord} `);
+            alert(`Trouvé ! : ${guessWord} `);
             if(finish.length === WORD.length){
-                alert(`gagné ${guessWord} en ${count} coups !`);
+                alert(`Vous avez trouvé ${guessWord} en ${count} coups !`);
                 game();
             }
         }
         else{
+            for(let j= 0; j <= fail.length; j++){
+                if(fail[j] === enterLetter){
+                alert("Lettre déjà utilisée, veuillez entrer une autre lettre.");
+                guessLetter();
+                count ++;
+            }
+        }
             alert(`raté : ${guessWord} `);
+            fail.splice(1, 0, enterLetter);
+            console.log("Lettres ratées: " +fail)
         }
         count ++;
         guessLetter();
